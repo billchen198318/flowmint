cf663a18396eb57df2972f7c6e551c8e11916ee0
billchen198318 <chen.xin.nien@gmail.com>
2026-08-27 21:44:17 +0800

    update

--- backend/app/src/main/java/org/qifu/fm/mapper/FmAiAnalysisMapper.java
+++ backend/app/src/main/java/org/qifu/fm/mapper/FmAiAnalysisMapper.java
@@ -1,6 +1,22 @@
 /*
  * Copyright 2019-2021 qifu of copyright Chen Xin Nien
  * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------------
+ *
+ * author: Chen Xin Nien
+ * contact: chen.xin.nien@gmail.com
+ *
  */
 package org.qifu.fm.mapper;
 
@@ -13,11 +29,12 @@ import org.qifu.fm.entity.FmAiAnalysis;
 @Mapper
 public interface FmAiAnalysisMapper extends IBaseMapper<FmAiAnalysis, String> {
 
-	FmAiAnalysis findLatestSucceeded(Map<String, Object> paramMap);
+	public FmAiAnalysis findLatestSucceeded(Map<String, Object> paramMap);
 
-	Integer findNextGenerationNo(Map<String, Object> paramMap);
+	public Integer findNextGenerationNo(Map<String, Object> paramMap);
 
-	int complete(Map<String, Object> paramMap);
+	public int complete(Map<String, Object> paramMap);
 
-	int fail(Map<String, Object> paramMap);
+	public int fail(Map<String, Object> paramMap);
+
 }
